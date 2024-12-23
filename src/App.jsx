0ea5cd57abe69889d3dcf90887ff1bae54adf785
Yash Murudkar <Yash.Murudkar@iss-stoxx.com>
2024-12-23 17:07:07 +0530

    add: loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import MovieInfo from './components/MovieInfo';
-import Movies from './components/Movies';
-import Placeholder from './components/Placeholder';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MovieInfo from "./components/MovieInfo";
+import Movies from "./components/Movies";
+import Placeholder from "./components/Placeholder";
 function App() {
-
-  const [search, setSearch] = useState("")
-  const [selectedMovie, setSelectedMovie] = useState(null)
-
-
+  const [search, setSearch] = useState("");
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  
   return (
     <>
-      <Navbar setSearch={setSearch} setSelectedMovie={setSelectedMovie}/>
-      {selectedMovie && <MovieInfo selectedMovie={selectedMovie} setSelectedMovie={setSelectedMovie}/>}
+      <Navbar setSearch={setSearch} setSelectedMovie={setSelectedMovie} />
+      {selectedMovie && (
+        <MovieInfo
+          selectedMovie={selectedMovie}
+          setSelectedMovie={setSelectedMovie}
+        />
+      )}
 
-      {search.length > 0 ?
-        <Movies search={search} setSelectedMovie={setSelectedMovie} /> :
-        <Placeholder />}
+      {search.length > 0 ? (
+        <Movies
+          search={search}
+          setSelectedMovie={setSelectedMovie}
+        />
+      ) : (
+        <Placeholder />
+      )}
     </>
   );
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,38 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import '../Movies.css'
-import Movielist from './Movielist'
+import React, { useEffect, useState } from "react";
+import "../Movies.css";
+import Movielist from "./Movielist";
 
-function Movies({ search , setSelectedMovie }) {
+function Movies({ search, setSelectedMovie }) {
+  const removeSpace = (word) => {
+    let str = "";
+    str = word.trim();
+    return str;
+  };
 
-    const removeSpace = (word) =>{
-      let str= ""
-      str = word.trim()
-      return str
-    }
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [data, setData] = useState([])
-    const url = `https://www.omdbapi.com/?s=${removeSpace(search)}&apikey=9d53487b` 
+  const url = `https://www.omdbapi.com/?s=${removeSpace(
+    search
+  )}&apikey=9d53487b`;
 
-    const searchMovies = async() =>{
-      const response = await fetch(url)
-      const data = await response.json(response)
-      setData(data.Search)
-    }
-    
-    useEffect(()=>{
-      searchMovies()
-      // eslint-disable-next-line
-    },[search ])
+  const searchMovies = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const data = await response.json(response);
+    setData(data.Search);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    searchMovies();
+    // eslint-disable-next-line
+  }, [search]);
 
   return (
-    <div className='movies'>
-        {data?.map((movie)=>{
-            return <div key={movie.imdbID}>
-            <Movielist poster = {movie.Poster} id={movie.imdbID} title= {movie.Title} year= {movie.Year} type= {movie.Type} setSelectedMovie = {setSelectedMovie}/>
+    <div className="movies">
+      {loading ? (
+        <h1 className="loading">Loading...</h1>
+      ) : (
+        data?.map((movie) => {
+          return (
+            <div key={movie.imdbID}>
+              <Movielist
+                poster={movie.Poster}
+                id={movie.imdbID}
+                title={movie.Title}
+                year={movie.Year}
+                type={movie.Type}
+                setSelectedMovie={setSelectedMovie}
+              />
             </div>
-        })}
+          );
+        })
+      )}
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
